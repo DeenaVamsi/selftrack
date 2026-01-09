c8594760dc8fa9cd243f498c49d6f7ea6eb9a6d2
DeenaVamsi <vamsichalla272@gmail.com>
2026-01-09 20:39:03 +0530

    Fix Node 22 crash: remove wildcard route

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(clientBuildPath));
 
-  app.get("/*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(clientBuildPath, "index.html"));
   });
 }
