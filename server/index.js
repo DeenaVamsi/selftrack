ee8ca43f40ae7106f470aacb75dbb45001b159b4
DeenaVamsi <vamsichalla272@gmail.com>
2026-01-09 20:25:17 +0530

    Fix wildcard route for Node 22 compatibility

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(clientBuildPath));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(clientBuildPath, "index.html"));
   });
 }
